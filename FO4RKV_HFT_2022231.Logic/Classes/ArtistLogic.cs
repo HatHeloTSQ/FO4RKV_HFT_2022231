60d0371a8a119dd0872511a928962430b846e749
Karikó-Tóth Máté <ktmswpro@gmail.com>
2022-11-17 19:19:10 +0100

    Added non-crud method AverageAge to ArtistLogic

--- FO4RKV_HFT_2022231.Logic/Classes/ArtistLogic.cs
+++ FO4RKV_HFT_2022231.Logic/Classes/ArtistLogic.cs
@@ -1,4 +1,5 @@
-﻿using FO4RKV_HFT_2022231.Models;
+﻿using FO4RKV_HFT_2022231.Logic.Interface;
+using FO4RKV_HFT_2022231.Models;
 using FO4RKV_HFT_2022231.Repository.RepositoryInterfaces;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace FO4RKV_HFT_2022231.Logic.Classes
 {
-    public class ArtistLogic
+    public class ArtistLogic : IArtistLogic
     {
         IRepository<Artist> artistrepo;
         public ArtistLogic(IRepository<Artist> artistrepo)
@@ -39,6 +40,12 @@ namespace FO4RKV_HFT_2022231.Logic.Classes
         public void Update(Artist item)
         {
             this.artistrepo.Update(item);
+        }
+
+        //non-crud methods
+        public double? AverageAge()
+        {
+            return this.artistrepo.ReadAll().Average(artist => artist.Age);
         }
     }
 }
