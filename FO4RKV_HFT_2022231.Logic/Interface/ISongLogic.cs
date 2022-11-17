8f355baf8f2e8d2c91c8ceafb198e7b6bfa5450b
Karikó-Tóth Máté <ktmswpro@gmail.com>
2022-11-17 19:16:59 +0100

    Added interfaces to Logic layer

---
+++ FO4RKV_HFT_2022231.Logic/Interface/ISongLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO4RKV_HFT_2022231.Logic.Interface
+{
+    public interface ISongLogic
+    {
+        int LongestSongArtist();
+    }
+}
