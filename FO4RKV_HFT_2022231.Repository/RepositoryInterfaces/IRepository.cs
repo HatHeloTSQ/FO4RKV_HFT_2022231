1fb98ea91e9ddb2c125f6155f413c5e9deac193e
Karikó-Tóth Máté <ktmswpro@gmail.com>
2022-11-17 16:47:15 +0100

    Created IRepository interface for inheritance of CRUD methods

---
+++ FO4RKV_HFT_2022231.Repository/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO4RKV_HFT_2022231.Repository.RepositoryInterfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
